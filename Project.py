f03b05d1df25befb7dea3c7f5fe337ae0f8a9e19
Andre17264 <119060202+Andre17264@users.noreply.github.com>
2022-12-19 15:00:44 +0100

    Update Project.py

--- Project.py
+++ Project.py
@@ -15,7 +15,7 @@ from keras.layers import LSTM, Dense
 from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 from sklearn.model_selection import TimeSeriesSplit, train_test_split
 
-# gather stock symbols from major US exchanges 
+# Gather stock symbols from major US exchanges 
 # Code taken from: https://levelup.gitconnected.com/how-to-get-all-stock-symbols-a73925c16a1b
 df1 = pd.DataFrame( si.tickers_sp500() )
 df2 = pd.DataFrame( si.tickers_nasdaq() )
@@ -28,10 +28,10 @@ sym2 = set( symbol for symbol in df2[0].values.tolist() )
 sym3 = set( symbol for symbol in df3[0].values.tolist() )
 sym4 = set( symbol for symbol in df4[0].values.tolist() )
 
-# Join the 4 sets into one. Because it's a set, there will be no duplicate symbols
+# Join the four sets into one. Because it is a set, there will be no duplicate symbols
 symbols = set.union( sym1, sym2, sym3, sym4 )
 
-# Some stocks are 5 characters. Those stocks with the suffixes listed below are not of interest.
+# Some stocks are five characters. Those stocks with the suffixes listed below are not of interest
 my_list = ['W', 'R', 'P', 'Q']
 del_set = set()
 sav_set = set()
